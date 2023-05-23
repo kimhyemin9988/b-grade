32c374be29fa1c7676b40b70189b7af034dcbe63
kimhyemin9988 <rlagpals159@naver.com>
2023-05-24 03:19:38 +0900

    Move code to Overlay Component

--- src/MovieF/Movie.tsx
+++ src/MovieF/Movie.tsx
@@ -228,22 +228,6 @@ export const BoxModal = styled(motion.div)`
   }
 `;
 
-export const overlay = {
-  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
-  visible: { backgroundColor: "#3b3636c5" },
-  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
-};
-
-export const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 0;
-  z-index: 15;
-`;
 
 export const BigCover = styled.div<{ bgPhoto: string | undefined }>`
   width: 100%;
