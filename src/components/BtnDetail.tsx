273fd5dd641dd8e391fbe6a604adad0ec44db62e
kimhyemin9988 <rlagpals159@naver.com>
2023-05-24 20:39:04 +0900

    refactor: change style

--- src/components/BtnDetail.tsx
+++ src/components/BtnDetail.tsx
@@ -1,13 +1,16 @@
 import SmallArrowBtn from "./SmallArrowBtn";
 import { LinkStyle } from "../App";
+import { Link } from "react-router-dom";
 
 const BtnDetail = ({ dataType, contentId }: { dataType: string | undefined, contentId: number | undefined }) => {
   return (
-    <LinkStyle to={dataType === "movie"
-      ? `${dataType}/${contentId}/details`
-      : `${contentId}/details`}>
-      <SmallArrowBtn></SmallArrowBtn>
-    </LinkStyle >
+    <div style={{ display: "flex"}}>
+      <LinkStyle to={dataType === "movie"
+        ? `${dataType}/${contentId}/details`
+        : `${contentId}/details`}>
+        <SmallArrowBtn></SmallArrowBtn>
+      </LinkStyle >
+    </div>
   );
 };
 export default BtnDetail;
