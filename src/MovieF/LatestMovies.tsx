5e67eb5a415d60b63d995ac84c60168e1817085f
kimhyemin9988 <rlagpals159@naver.com>
2023-05-30 14:06:39 +0900

    refactor: Change to media query

--- src/MovieF/LatestMovies.tsx
+++ src/MovieF/LatestMovies.tsx
@@ -2,15 +2,13 @@ import { latestMovies } from "../api";
 import { useQuery } from "react-query";
 import {
   movieData,
-  Overview,
-  RatingSpan,
   smallVideo,
-  Wrapper,
 } from "./Movie";
 import styled from "styled-components";
 import YouTube from "react-youtube";
 import LoadingC from "../components/LoadingC";
 import BtnDetail from "../components/BtnDetail";
+import { Overview } from "./Movie";
 
 export const Container = styled.section<{ bgPhoto: string | undefined }>`
   width: 100%;
@@ -44,6 +42,10 @@ const SqureBox = styled.article<{ posterbg: string | undefined }>`
   height: 6rem;
   background-image: url(${(props) => props.posterbg});
   background-size: 100% 100%;
+  grid-area: posterbg;
+  @media screen and (max-width: 550px) {
+    margin-left: 10px;
+  }
 `;
 export const OverviewContainer = styled.div`
   grid-area: overview;
@@ -57,6 +59,44 @@ export const xLarge = {
   width: "528",
 };
 
+const YouTubeStyle = styled(YouTube)`
+  height: 300px;
+  padding-left: 20px;
+  grid-area: video;
+  @media screen and (max-width: 550px) {
+    padding-left: 10px;
+  }
+`
+const LatestOriginal = styled.p`
+  padding-left: 20px;
+  font-size: 0.5rem;
+  font-weight: 600;
+@media screen and (max-width: 550px) {
+    padding-left: 10px;
+  }
+`
+const LatestTitle = styled.p`
+    margin-bottom: 20px;
+    padding-left: 20px;
+    font-size: 0.5rem;
+    font-weight: 600;
+  @media screen and (max-width: 550px) {
+    margin-bottom: 10px;
+    padding-left: 10px;
+  }
+`
+const LatestWrapper = styled.div`
+  color: ${(props) => props.theme.bodyFtColor};
+  flex-direction: column;
+  align-items: flex-start;
+  display: flex;
+  min-height: 30vh;
+  margin-bottom: 180px;
+  @media screen and (max-width: 550px) {
+    margin-bottom: 20px;
+  }
+`
+
 const LatestMovies = ({ dataType }: { dataType: string }) => {
   /* 데이터 받아오기 */
   const { isLoading, data } = useQuery<movieData[]>(
@@ -67,78 +107,33 @@ const LatestMovies = ({ dataType }: { dataType: string }) => {
     <>
       {isLoading ? (
         <LoadingC></LoadingC>
-      ) : window.outerWidth <= 550 ? (
-        <Wrapper style={{ alignItems: "flex-start", marginBottom: "20px" }}>
-          <RatingSpan style={{ paddingLeft: "10px", marginBottom: "10px" }}>
+      ) :
+        <LatestWrapper>
+          <LatestTitle>
             Popular Movies In Theaters
-          </RatingSpan>
+          </LatestTitle>
           <Container
-            bgPhoto={`https://image.tmdb.org/t/p/original/${data?.[0].backdrop_path}`}
-          >
+            bgPhoto={`https://image.tmdb.org/t/p/original/${data?.[0].backdrop_path}`}>
             <Blur>
-              <RatingSpan
-                style={{
-                  gridArea: "title",
-                  alignSelf: "center",
-                  paddingLeft: "10px",
-                }}
-              >
-                {data?.[0].original_title}
+              <div style={{ display: "flex", alignItems:"center", gridArea:"title" }}>
+                <LatestOriginal>
+                  {data?.[0].original_title}
+                </LatestOriginal>
                 <BtnDetail dataType={dataType} contentId={data?.[0].id}></BtnDetail>
-              </RatingSpan>
+              </div>
               <SqureBox
-                style={{ gridArea: "posterbg", marginLeft: "10px" }}
                 posterbg={`https://image.tmdb.org/t/p/w300/${data?.[0].poster_path}`}
               ></SqureBox>
-              <YouTube
-                style={{ gridArea: "video", paddingLeft: "10px" }}
+              <YouTubeStyle
                 videoId={data?.[1].key}
-                opts={smallVideo}/>
+                opts={window.outerWidth <= 550 ? smallVideo : xLarge} />
               <OverviewContainer>
                 <Overview>{data?.[0].overview}</Overview>
               </OverviewContainer>
             </Blur>
           </Container>
-        </Wrapper>
-      ) : (
-        <Wrapper style={{ alignItems: "flex-start", marginBottom: "180px" }}>
-          <RatingSpan style={{ paddingLeft: "20px", marginBottom: "20px" }}>
-            Popular Movies In Theaters
-          </RatingSpan>
-          <Container
-            bgPhoto={`https://image.tmdb.org/t/p/original/${data?.[0].backdrop_path}`}
-          >
-            <Blur>
-              <RatingSpan
-                style={{
-                  gridArea: "title",
-                  alignSelf: "center",
-                  paddingLeft: "20px",
-                }}
-              >
-                {data?.[0].original_title}
-                <BtnDetail dataType={dataType} contentId={data?.[0].id}></BtnDetail>
-              </RatingSpan>
-              <SqureBox
-                style={{ gridArea: "posterbg" }}
-                posterbg={`https://image.tmdb.org/t/p/w300/${data?.[0].poster_path}`}
-              ></SqureBox>
-                <YouTube
-                  style={{
-                    gridArea: "video",
-                    height: "300px",
-                    paddingLeft: "20px",
-                  }}
-                  videoId={data?.[1].key}
-                  opts={xLarge}
-                />
-              <OverviewContainer>
-                <Overview>{data?.[0].overview}</Overview>
-              </OverviewContainer>
-            </Blur>
-          </Container>
-        </Wrapper>
-      )}
+        </LatestWrapper>
+      }
     </>
   );
 };
