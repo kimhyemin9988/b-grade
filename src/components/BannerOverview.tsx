9cf7f9690b0f03b26f771d0ad479440492d349d4
kimhyemin9988 <rlagpals159@naver.com>
2023-06-01 15:27:36 +0900

    refactor: Add code When there is no overview

--- src/components/BannerOverview.tsx
+++ src/components/BannerOverview.tsx
@@ -16,6 +16,7 @@ export const Banner = styled.div<{ bgPhoto: string | undefined }>`
   height: 75vh;
   margin-top: 13vh;
   font-weight: 600;
+  width: 80%;
   @media screen and (max-width: 550px) {
     margin-top: 8vh;
     height: 33vh;
@@ -23,19 +24,21 @@ export const Banner = styled.div<{ bgPhoto: string | undefined }>`
 `;
 
 const BannerOverview = ({ content, sliceLength, dataType }: { content: { overview: string, title?: string, id: number, backdrop_path: string, name?: string } | undefined, sliceLength: number, dataType: string | undefined }) => {
-    return (
-        <Banner
-            bgPhoto={`https://image.tmdb.org/t/p/original/${content?.backdrop_path}`}>
-            <Title>{content?.title === undefined ? content?.name : content?.title}</Title>
-            <Overview>
-                {content?.overview.slice(0,
-                    content?.overview.indexOf(" ", sliceLength)
-                )}
-                {content && content?.overview.length > sliceLength ? "..." : "."}
-            </Overview>
-            <BtnDetail dataType={dataType} contentId={content?.id}></BtnDetail>
-        </Banner>
-    )
+  return (
+    <Banner
+      bgPhoto={`https://image.tmdb.org/t/p/original/${content?.backdrop_path}`}>
+      <Title>{content?.title === undefined ? content?.name : content?.title}</Title>
+      {content?.overview !== "" &&
+        <Overview>
+          {content?.overview.slice(0,
+            content?.overview.indexOf(" ", sliceLength)
+          )}
+          {content && content?.overview.length > sliceLength ? "..." : "."}
+        </Overview>
+      }
+      <BtnDetail dataType={dataType} contentId={content?.id}></BtnDetail>
+    </Banner>
+  )
 };
 
 export default BannerOverview;
