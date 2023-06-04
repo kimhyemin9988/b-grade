52a79f4566f9d76605aac8b1f4583e85c6469580
kimhyemin9988 <rlagpals159@naver.com>
2023-06-04 23:45:08 +0900

    refactor: Unify redundant code

--- src/api.tsx
+++ src/api.tsx
@@ -1,18 +1,33 @@
 import { movieData } from "./MovieF/Movie";
 import { LatestShowsData } from "./Tv/LatestTopShows";
 
+/**** 오늘 날짜 불러오기 ****/
+const fullDate = new Date();
+const alertDayLenth = (number : number) => {
+  let dayNumber = JSON.stringify(number);
+  if (dayNumber.length === 1) {
+    dayNumber = 0 + dayNumber;
+  }
+  return dayNumber;
+}
+const year = JSON.stringify(fullDate.getFullYear());
+const month = alertDayLenth(fullDate.getMonth() + 1);
+const day = alertDayLenth(fullDate.getDate());
+const full_date = [year, month, day].join("-");
 
+/**** DB 키 ****/
 export const dbApiKey = process.env.REACT_APP_DB_API_KEY;
 
+/**** 배경, 포스터, 개요 없는것 삭제 ****/
 const removeData = (json: any) => {
   return json.results
-    .filter((i: movieData) => i.poster_path !== null && i.backdrop_path !== null && i.overview !== "");
+    .filter((i: movieData | LatestShowsData) => i.poster_path !== null && i.backdrop_path !== null && i.overview !== "");
 }
 
 const movieList = async () => {
   let page = 1;
   let dataArray: [] = [];
-  while (page < 3) {
+  while (dataArray.length <= 25) {
     const response = await fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${dbApiKey}&sort_by=release_date.desc&include_adult=false&include_video=false&page=${page}&vote_average.gte=4.5&vote_average.lte=5.5&with_watch_monetization_types=flatrat&include_video=false`
     );
@@ -21,7 +36,8 @@ const movieList = async () => {
     page++;
     dataArray = [...dataArray, ...data];
   }
-  return dataArray;
+  const resultArray = dataArray.slice(0, 25);
+  return resultArray;
 };
 export { movieList };
 
@@ -68,77 +84,63 @@ const tvPopular = async () => {
 
   return newDataArray;
 };
+
 export { tvPopular };
 
 const tvAiring = async () => {
   let page = 1;
   let dataArray: [] = [];
-  while (page < 6) {
+  while (dataArray.length <= 25) {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/on_the_air?api_key=${dbApiKey}&language=en-US&page=${page}`
+      `https://api.themoviedb.org/3/tv/on_the_air?api_key=${dbApiKey}&page=${page}`
     );
     const json = await response.json();
     const data: [] = await removeData(json);
     page++;
     dataArray = [...dataArray, ...data];
   }
-  return dataArray;
+  const resultArray = dataArray.slice(0, 25);
+  return resultArray;
 };
 export { tvAiring };
 
 const tvTopRated = async () => {
   let page = 1;
   let dataArray: [] = [];
-  while (page < 6) {
+  while (dataArray.length <= 25) {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/top_rated?api_key=${dbApiKey}&language=en-US&page=${page}`
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=${dbApiKey}&page=${page}`
     );
     const json = await response.json();
     const data: [] = await removeData(json);
     page++;
     dataArray = [...dataArray, ...data];
   }
-  return dataArray;
+  const resultArray = dataArray.slice(0, 25);
+  return resultArray;
 };
 export { tvTopRated };
 
 const tvLatest = async () => {
   let page = 1;
   const dataArray: LatestShowsData[] = [];
-  while (page < 6) {
+
+  while (dataArray.length === 0) {
     const response = await fetch(
-      `https://api.themoviedb.org/3/tv/popular?api_key=${dbApiKey}&page=${page}`
+      `https://api.themoviedb.org/3/tv/popular?api_key=${dbApiKey}&page=${page}&first_air_date.gte=${full_date}`
     );
     const json = await response.json();
-    const data: LatestShowsData[] = await json.results.filter(
-      (i: LatestShowsData) => i.poster_path !== null
-    );
+    const data: LatestShowsData[] = await removeData(json);
     page++;
     dataArray.push(...data);
   }
-  const videoResponse = await fetch(
-    `https://api.themoviedb.org/3/tv/${dataArray?.[0].id}?api_key=${dbApiKey}`
-  );
-  const videoJson = await videoResponse.json();
-  dataArray.push(videoJson);
-  const lastData = dataArray.filter(
-    (i: LatestShowsData) => i.id === dataArray?.[0].id
-  );
-
-  return lastData;
+  return dataArray;
 };
 export { tvLatest };
 
 /******************* movie *******************/
 
 // 상영중인 영화중 최신의 인기있는 영화
-
-const fullDate = new Date();
-
-const year = JSON.stringify(fullDate.getFullYear());
-const month = JSON.stringify(fullDate.getMonth() + 1);
-const dateArray = [year, month];
-const date = dateArray.join("-");
 
 interface videoData {
   name: string;
@@ -147,7 +149,7 @@ interface videoData {
 
 const latestMovies = async () => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${dbApiKey}&sort_by=popularity.asc&include_adult=false&page=1&release_date.lte=${date}&with_watch_monetization_types=flatrate`
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${dbApiKey}&sort_by=popularity.asc&include_adult=false&page=1&release_date.lte=${full_date}&with_watch_monetization_types=flatrate`
   );
   const json = await response.json();
   const data = await removeData(json).slice(0, 1);
@@ -166,12 +168,12 @@ export { latestMovies };
 
 /* upcoming, topRated는 data의 양이 많지 않아 여러 page의 정보를 받으면 반복해서 똑같은 정보가 쌓임 */
 
-const topRatedMovies = async () => {
+const topAndUpcomingMovies = async (url: string) => {
   let page = 1;
   let dataArray: [] = [];
   while (page < 2) {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=${dbApiKey}&${page}`
+      `https://api.themoviedb.org/3/movie/${url}?api_key=${dbApiKey}&${page}`
     );
     const json = await response.json();
     const data: [] = await removeData(json);
@@ -180,23 +182,8 @@ const topRatedMovies = async () => {
   }
   return dataArray;
 };
-export { topRatedMovies };
+export { topAndUpcomingMovies };
 
-const upcomingMovies = async () => {
-  let page = 1;
-  let dataArray: [] = [];
-  while (page < 2) {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${dbApiKey}&${page}`
-    );
-    const json = await response.json();
-    const data: [] = await removeData(json);
-    page++;
-    dataArray = [...dataArray, ...data];
-  }
-  return dataArray;
-};
-export { upcomingMovies };
 
 const SearchData = async (keyword: string | null, page: number) => {
   let newPage = page;
